085eb2cafa1ba6744c7344e1b1985391a798a5a9
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-11-24 16:23:57 +0200

    fix: add changes to workflow file and app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.json({ message: "working!" });
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port port ${port}`);
